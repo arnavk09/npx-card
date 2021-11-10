4c441ee11ea847d39aac84ef639fa66177c525d3
Arnav Katgeri <arnav02@outlook.com>
2021-11-10 22:08:52 +0530

    made card

--- card.js
+++ card.js
@@ -1,32 +1,33 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
-const boxen = require('boxen');
-const chalk = require('chalk');
-const clear = require('clear');
+const boxen = require("boxen");
+const chalk = require("chalk");
+const clear = require("clear");
 
 clear();
 
 const data = {
-  name: chalk.bold.green('                    Cristian Del Rio'),
-  handle: chalk.white('@cristianjdelrio'),
-  work: `${chalk.white('Backend Developer at')} ${chalk
-    .hex('#cca918')
-    .bold('Condor Labs')}`,
-  github: chalk.gray('https://github.com/') + chalk.green('cristianjdelrio'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('cristianjdelrio'),
+  name: chalk.bold.green("                    Arnav Katgeri"),
+  handle: chalk.white("@arnavk09"),
+  work: `${chalk.white("Web Developer and Computer Science Sophomore")} ${chalk
+    .hex("#cca918")
+    .bold("College : Manipal University Jaipur")}`,
+  github: chalk.gray("https://github.com/") + chalk.green("arnavk09"),
+  twitter: chalk.gray("https://twitter.com/") + chalk.cyan("KatgeriArnav"),
   linkedin:
-    chalk.gray('https://linkedin.com/in/') + chalk.blue('cristianjdelrio'),
-  web: chalk.cyan('https://cristianjdelrio.com'),
-  npx: chalk.red('npx') + ' ' + chalk.white('cristianjdelrio'),
+    chalk.gray("https://linkedin.com/in/") +
+    chalk.blue("arnav-katgeri-6159361b4/"),
+  web: chalk.cyan("arnavk09.github.io/personalsite/"),
+  npx: chalk.red("npx") + " " + chalk.white("arnavk09"),
 
-  labelWork: chalk.white.bold('       Work:'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWeb: chalk.white.bold('        Web:'),
-  labelCard: chalk.white.bold('       Card:'),
+  labelWork: chalk.white.bold("       College:"),
+  labelTwitter: chalk.white.bold("    Twitter:"),
+  labelGitHub: chalk.white.bold("     GitHub:"),
+  labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+  labelWeb: chalk.white.bold("        Web:"),
+  labelCard: chalk.white.bold("       Card:"),
 };
 
 const me = boxen(
@@ -42,23 +43,21 @@ const me = boxen(
     ``,
     `${data.labelCard}  ${data.npx}`,
     ``,
-    `${chalk.bold(
-      "- As Obi Wan says - Hello There! I'm a passionated MERN stack"
-    )}`,
-    `${chalk.bold(
-      "developer, I'm from Colombia and I'm in love with 3 things:"
-    )}`,
+    `${chalk.bold("-  Hello There! I'm a Web Developer in the making and ")}`,
+    `${chalk.bold("I'm from India and I'm usually seen doing 3 things:")}`,
     ``,
-    `${chalk.bold('1. Learning new Things 🧠')}`,
-    `${chalk.bold('2. Inviting people to know the tech world 💻')}`,
-    `${chalk.bold('3. My family 👪')}`,
-  ].join('\n'),
+    `${chalk.bold("1. Polishing my Data Structures skills 🧠")}`,
+    `${chalk.bold(
+      "2. Learning about new technologies and implementing all I can 💻"
+    )}`,
+    `${chalk.bold("3. Pizza and a good book 👪")}`,
+  ].join("\n"),
   {
     margin: 1,
-    float: 'center',
+    float: "center",
     padding: 1,
-    borderStyle: 'single',
-    borderColor: 'blue',
+    borderStyle: "single",
+    borderColor: "blue",
   }
 );
 
